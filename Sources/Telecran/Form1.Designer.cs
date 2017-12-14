7f263ae798ec6d896a033f433ac2a728195230ce
Marc Silanus <msilanus@gmail.com>
2017-12-14 12:58:04 +0100

    Add fonctionalities
    like pen color and width, clear screen, ...

--- Sources/Telecran/Form1.Designer.cs
+++ Sources/Telecran/Form1.Designer.cs
@@ -28,24 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.couleurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enregistrerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinceauToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.décalageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
+            this.effacerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.couleurToolStripMenuItem,
+            this.enregistrerToolStripMenuItem,
+            this.pinceauToolStripMenuItem,
+            this.décalageToolStripMenuItem,
+            this.effacerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1228, 28);
+            this.menuStrip1.TabIndex = 1001;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // couleurToolStripMenuItem
+            // 
+            this.couleurToolStripMenuItem.Name = "couleurToolStripMenuItem";
+            this.couleurToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.couleurToolStripMenuItem.Text = "Couleur";
+            this.couleurToolStripMenuItem.Click += new System.EventHandler(this.couleurToolStripMenuItem_Click);
+            // 
+            // enregistrerToolStripMenuItem
+            // 
+            this.enregistrerToolStripMenuItem.Name = "enregistrerToolStripMenuItem";
+            this.enregistrerToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.enregistrerToolStripMenuItem.Text = "Enregistrer";
+            this.enregistrerToolStripMenuItem.Click += new System.EventHandler(this.enregistrerToolStripMenuItem_Click);
+            // 
+            // pinceauToolStripMenuItem
+            // 
+            this.pinceauToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8});
+            this.pinceauToolStripMenuItem.Name = "pinceauToolStripMenuItem";
+            this.pinceauToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.pinceauToolStripMenuItem.Text = "Pinceau";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem2.Text = "-3";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem3.Text = "-2";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem4.Text = "-1";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Checked = true;
+            this.toolStripMenuItem5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem5.Text = "0";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem6.Text = "+1";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem7.Text = "+2";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(96, 24);
+            this.toolStripMenuItem8.Text = "+3";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // décalageToolStripMenuItem
+            // 
+            this.décalageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem9,
+            this.toolStripMenuItem10,
+            this.toolStripMenuItem11,
+            this.toolStripMenuItem12,
+            this.toolStripMenuItem13});
+            this.décalageToolStripMenuItem.Name = "décalageToolStripMenuItem";
+            this.décalageToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.décalageToolStripMenuItem.Text = "Décalage";
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(94, 24);
+            this.toolStripMenuItem9.Text = "1";
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.toolStripMenuItem9_Click);
+            // 
+            // toolStripMenuItem10
+            // 
+            this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(94, 24);
+            this.toolStripMenuItem10.Text = "5";
+            this.toolStripMenuItem10.Click += new System.EventHandler(this.toolStripMenuItem10_Click);
+            // 
+            // toolStripMenuItem11
+            // 
+            this.toolStripMenuItem11.Checked = true;
+            this.toolStripMenuItem11.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(94, 24);
+            this.toolStripMenuItem11.Text = "10";
+            this.toolStripMenuItem11.Click += new System.EventHandler(this.toolStripMenuItem11_Click);
+            // 
+            // toolStripMenuItem12
+            // 
+            this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(94, 24);
+            this.toolStripMenuItem12.Text = "20";
+            this.toolStripMenuItem12.Click += new System.EventHandler(this.toolStripMenuItem12_Click);
+            // 
+            // toolStripMenuItem13
+            // 
+            this.toolStripMenuItem13.Name = "toolStripMenuItem13";
+            this.toolStripMenuItem13.Size = new System.Drawing.Size(94, 24);
+            this.toolStripMenuItem13.Text = "30";
+            this.toolStripMenuItem13.Click += new System.EventHandler(this.toolStripMenuItem13_Click);
+            // 
+            // effacerToolStripMenuItem
+            // 
+            this.effacerToolStripMenuItem.Name = "effacerToolStripMenuItem";
+            this.effacerToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.effacerToolStripMenuItem.Text = "Effacer";
+            this.effacerToolStripMenuItem.Click += new System.EventHandler(this.effacerToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1027, 547);
+            this.ClientSize = new System.Drawing.Size(1228, 666);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Telecran C#";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem couleurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enregistrerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinceauToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem décalageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem13;
+        private System.Windows.Forms.ToolStripMenuItem effacerToolStripMenuItem;
 
     }
 }
--- Sources/Telecran/Form1.cs
+++ Sources/Telecran/Form1.cs
@@ -13,8 +13,9 @@ namespace dessin
     {
         private Graphics g;
         private Bitmap b;
-        private Pen myPen = new Pen(Color.Red, 3);
+        private Pen myPen = new Pen(Color.Red, 4);
         private Point p1, p2;
+        private int x,y,shift=10;
         
         public Form1()
         {
@@ -25,7 +26,9 @@ namespace dessin
         {
             b = new Bitmap(Width, Height);
             g = Graphics.FromImage(b);
-            p1 = new Point(this.Width / 2, this.Height / 2);
+            x=this.Width / 2;
+            y = this.Height / 2;
+            p1 = new Point(x, y);
             p2 = new Point();
             p2 = p1;
             this.KeyPreview = true;
@@ -38,40 +41,56 @@ namespace dessin
 
         private void toUp()
         {
-            p2.Y -= 10;
-            g.DrawLine(myPen, p1, p2);
-            p1 = p2;
-            Invalidate();
+            y = p2.Y - shift;
+            if (y > 30)
+            {
+                p2.Y = y;
+                g.DrawLine(myPen, p1, p2);
+                p1 = p2;
+                Invalidate();
+            }
         }
 
         
 
         private void toLeft()
         {
-            p2.X -= 10;
-            g.DrawLine(myPen, p1, p2);
-            p1 = p2;
-            Invalidate();
+            x = p2.X - shift;
+            if (x > 0)
+            {
+                p2.X = x;
+                g.DrawLine(myPen, p1, p2);
+                p1 = p2;
+                Invalidate();
+            }
         }
 
         
 
         private void toDown()
         {
-            p2.Y += 10;
-            g.DrawLine(myPen, p1, p2);
-            p1 = p2;
-            Invalidate();
+            y = p2.Y + shift;
+            if (y < this.Height - 45)
+            {
+                p2.Y = y;
+                g.DrawLine(myPen, p1, p2);
+                p1 = p2;
+                Invalidate();
+            }
         }
 
         
 
         private void toRight()
         {
-            p2.X += 10;
-            g.DrawLine(myPen, p1, p2);
-            p1 = p2;
-            Invalidate();
+            x = p2.X + shift;
+            if (x < this.Width - 18)
+            {
+                p2.X = x;
+                g.DrawLine(myPen, p1, p2);
+                p1 = p2;
+                Invalidate();
+            }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -92,5 +111,137 @@ namespace dessin
             g.Clear(this.BackColor);
 
         }
+
+        
+        private void couleurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.Color = myPen.Color;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                myPen.Color = MyDialog.Color;
+            }
+        }
+
+        private void enregistrerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog mySaveFileDialog = new SaveFileDialog();
+            mySaveFileDialog.Filter = "Image bmp (*.bmp) | *.bmp";
+            if (mySaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                b.Save(mySaveFileDialog.FileName);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            myPen.Width =1;
+            checkItems(toolStripMenuItem2);
+        }
+
+        private void checkItems(ToolStripMenuItem item)
+        {
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = false;
+            toolStripMenuItem7.Checked = false;
+            toolStripMenuItem8.Checked = false;
+            item.Checked = true;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 2;
+            checkItems(toolStripMenuItem3);
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 3;
+            checkItems(toolStripMenuItem4);
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 4;
+            checkItems(toolStripMenuItem5);
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 5;
+            checkItems(toolStripMenuItem6);
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 6;
+            checkItems(toolStripMenuItem7);
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            myPen.Width = 7;
+            checkItems(toolStripMenuItem8);
+        }
+
+        private void shiftItem(ToolStripMenuItem item)
+        {
+            toolStripMenuItem9.Checked = false;
+            toolStripMenuItem10.Checked = false;
+            toolStripMenuItem11.Checked = false;
+            toolStripMenuItem12.Checked = false;
+            toolStripMenuItem13.Checked = false;
+            
+            item.Checked = true;
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+            shift = 1;
+            shiftItem(toolStripMenuItem9);
+        }
+
+        private void toolStripMenuItem10_Click(object sender, EventArgs e)
+        {
+            shift = 5;
+            shiftItem(toolStripMenuItem10);
+        }
+
+        private void toolStripMenuItem11_Click(object sender, EventArgs e)
+        {
+            shift = 10;
+            shiftItem(toolStripMenuItem11);
+        }
+
+        private void toolStripMenuItem12_Click(object sender, EventArgs e)
+        {
+            shift = 20;
+            shiftItem(toolStripMenuItem12);
+        }
+
+        private void toolStripMenuItem13_Click(object sender, EventArgs e)
+        {
+            shift = 30;
+            shiftItem(toolStripMenuItem13);
+        }
+
+        private void effacerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Clear(this.BackColor);
+            x = this.Width / 2;
+            y = this.Height / 2;
+            p2.X = x; p2.Y = y;
+            p1 = p2;
+            g.DrawLine(myPen, p1, p2);
+            Invalidate();
+        }
+
+       
+
+        
     }
 }
